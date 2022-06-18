17ea98443ba994f9b8a1a83364eac2551ac53822
Sarah Moss <sarah-moss@live.com>
2022-06-18 14:01:07 +0000

    Refactor students.js + teachers.js to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,7 +1,6 @@
 const { Pool } = require('pg');
 
 // Using a Pool instead of Client is the preferred way to query with node-postgres. A Pool will manage multiple client connections for us which we don't really need to worry about right now. Just know that either could be used, but Pool is preferred.
-
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -11,25 +10,19 @@ const pool = new Pool({
 
 pool.connect();
 
-// pool.query(`
-// SELECT students.id, students.name, cohorts.name
-// FROM students
-//   JOIN cohorts ON cohorts.id = cohort_id
-// LIMIT 5;
-// `)
-// .then(res => {
-//   // console.log(res);
-//   console.log(res.rows)
-// })
-// .catch(err => console.error('query error', err.stack));
-
-pool.query(`
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+const query = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`
+
+pool.query(query, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -11,31 +11,19 @@ const pool = new Pool({
 
 pool.connect();
 
-// pool.query(`
-// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, COUNT(assistance_requests.*) as total_assistances
-// FROM teachers
-//   JOIN assistance_requests ON teacher_id = teachers.id
-//   JOIN students ON student_id = students.id
-//   JOIN cohorts ON cohort_id = cohorts.id
-// WHERE cohorts.name = '${process.argv[2]}'
-// GROUP BY teachers.name, cohorts.name
-// ORDER BY total_assistances DESC;
-// `)
-// .then(res => {
-//   res.rows.forEach(user => {
-//     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-//   })
-// }).catch(err => console.error('query error', err.stack));
-
-pool.query(`
+const cohortName = process.argv[2] || 'JUL02'
+const values = [cohortName];
+const query = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`
+
+pool.query(query, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
